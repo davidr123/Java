697dc514011957ac6d3172c190a3b5cc4c34a406
Andrea Jurado <avjh_18@hotmail.com>
2018-09-11 01:27:32 -0500

    jhvgjhvhj

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,14 @@ var router = express.Router();
 var fortune = require('../lib/fortune.js');
 
 
-
 router.get('/', function (req, res) {
-    res.render('index', {title: "Index Home"});
+    res.render('front/', {title: "Index Home"});
 });
+
+router.get('/inicio', function (req, res) {
+    res.render('index', {title: "Index"});
+});
+
 
 //formato reducido para renderizar la vista 
 router.get('/thank-you', (req, res) => res.render('thank-you') );
@@ -32,10 +36,12 @@ router.get('/login', function(req, res){
 router.post('/process', function(req, res){
   console.log('Form (from querystring): ' + req.query.form);
   console.log('CSRF token (from hidden form field): ' + req.body._csrf);
-  console.log('Name (from visible form field): ' + req.body.name);
-  console.log('Email (from visible form field): ' + req.body.email);
-  res.redirect(303, '/thank-you');
+  console.log('User (from visible form field): ' + req.body.usuario);
+  console.log('Password (from visible form field): ' + req.body.password);
+  res.redirect(303, '/index');
+  //res.redirect(303, '/thank-you');
 });
+
 
 
 module.exports = router;
