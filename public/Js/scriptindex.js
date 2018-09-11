6e7afdc645a131f75956709bac192879c94b1985
Andrea Jurado <avjh_18@hotmail.com>
2018-09-11 01:23:38 -0500

    hjbj

--- public/Js/scriptindex.js
+++ public/Js/scriptindex.js
@@ -1,69 +1,9 @@
-class Usuario{
-    constructor(user, pass, puntaje,rompecabeza){
-        this.user=user;
-        this.pass=pass;
-        this.puntaje=puntaje;
-        var arrRompecabezas=[];
-        rompecabeza.forEach( function(rompecbz){
-            arrRompecabezas.push(new Rompecabeza(rompecbz.piezas, rompecbz.espacios));
-        });
-        this.rompecabeza=arrRompecabezas;
-    }
-}
-
-class Rompecabeza{
-    constructor(piezas,espacios){
-        var arrPiezas=[];
-        piezas.forEach( function(pieza){
-            arrPiezas.push(new Piezas(pieza.id, pieza.posx,pieza.posy,pieza.url));
-        });
-        this.piezas=arrPiezas;
-                       
-        var arrEspacios=[];
-        espacios.forEach( function(espacio){
-            arrEspacios.push(new Espacios(espacio.id, espacio.posx,espacio.posy,espacio.url));
-        });               
-        this.espacios=arrEspacios;
-    }
-    
-}
-class Piezas {
-    constructor(id, posx, posy, url){
-        this.id = id;
-        this.posx = posx;
-        this.posy = posy;
-        this.url = url;
-    }
-}
-class Espacios {
-    constructor(id, posx, posy, url){
-        this.id = id;
-        this.posx = posx;
-        this.posy = posy;
-        this.url = url;
-    }
-}
-
-    var arrUsers= [];
-    
-    $.getJSON('ejemplo.json', function(data){
-        
-        $.each(data, function(i, resultado){
-          arrUsers.push(new Usuario(resultado.user, resultado.pass, resultado.puntaje, resultado.rompecabeza));
-        });
-        console.log(arrUsers);
-        var data= "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(arrUsers));
-        
-        $("#descargarjson").attr("href","data:"+data);
-        $("#descargarjson").attr("download","ejemplo.json");
-    });
-    
-
+/*
 $(".uno").click(function(){
     console.log("avatar uno");
     var flag= true;
 });
-/*
+
 function updateImg(num){
    if(num===1) {
        $('#avatarN').load("./avatarEscogido.html").add("<img src='./images/avatar1.png' />").appendTo(document.body);
@@ -71,14 +11,17 @@ function updateImg(num){
    }
 }*/
 
-var texto= document.getElementById("txtDescripcion");
+$("input").change(function(e) {
 
-function cargarImg() {
-    var x= document.createElement("img");
-    x.src= document.getElementById("imgURL").value;
-    x.alt= 'alt text';
-    document.getElementById("contenido1").appendChild(x);
-}
-
-btnCargarImagen.addEventListener('click', cargarImg);
-
+    for (var i = 0; i < e.originalEvent.srcElement.files.length; i++) {
+        
+        var file = e.originalEvent.srcElement.files[i];
+        var img = document.createElement("img");
+        var reader = new FileReader();
+        reader.onloadend = function() {
+             img.src = reader.result;
+        }
+        reader.readAsDataURL(file);
+        $("#contentImg").after(img);
+    }
+});
