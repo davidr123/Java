d97803dc70e06d181e6336b0788fd54badf0c4d9
Andrea Jurado <avjh_18@hotmail.com>
2018-09-11 03:10:19 -0500

    y no seeee que paso conmigo despues

--- public/Js/scriptindex.js
+++ public/Js/scriptindex.js
@@ -11,6 +11,31 @@ function updateImg(num){
    }
 }*/
 
+window.addEventListener('load', () => {
+				// noinspection JSUnresolvedVariable
+				let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+				let xhr = new XMLHttpRequest();
+				xhr.open('GET', '/audios/background.mp3');
+				xhr.responseType = 'arraybuffer';
+				xhr.addEventListener('load', () => {
+					let playsound = (audioBuffer) => {
+						let source = audioCtx.createBufferSource();
+						source.buffer = audioBuffer;
+						source.connect(audioCtx.destination);
+						source.loop = false;
+						source.start();
+
+						setTimeout(function () {
+							playsound(audioBuffer);
+						}, 1000 + Math.random()*2500);
+					};
+
+					audioCtx.decodeAudioData(xhr.response).then(playsound);
+				});
+				xhr.send();
+			});
+
+
 $("input").change(function(e) {
 
     for (var i = 0; i < e.originalEvent.srcElement.files.length; i++) {
